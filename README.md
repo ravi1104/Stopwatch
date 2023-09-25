49e25b74e4b63b9f35e86ec558dfb8dcb95ce5d1
ravvi1104 <rry4298@gmail.com>
2023-09-25 18:34:40 +0530

    html css js working fine

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Stopwatch
 
+A simple stopwatch web application built using HTML, CSS, and JavaScript.
+
+## Description
+
+This project is a web-based stopwatch that allows users to start, stop, and reset the timer. It features a clean and user-friendly interface.
+
+## Features
+
+- Start: Begin the stopwatch timer.
+- Stop: Pause the stopwatch timer.
+- Reset: Reset the stopwatch to zero.
+- Minutes and seconds input fields for custom timer duration.
+
+## Installation
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/ravi1104/Stopwatch.git
--- script.js
+++ script.js
@@ -1,62 +1,88 @@
-"use strict"
+"use strict";
+
+// Initialize timer variables
 var minutes = 0;
 var seconds = 0;
-var intervalId = null;
+var timeoutId = null;
+
+// Get DOM elements
 var startBtn = document.getElementById("startButton");
 var stopBtn = document.getElementById("stopButton");
 var resetBtn = document.getElementById("resetButton");
 var getMinutes = document.getElementById("minutes");
 var getSeconds = document.getElementById("seconds");
+var audio = new Audio('sound.mp3');
 
+// Function to update the timer display
 function updateTimerDisplay() {
     var formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
     var formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
     getMinutes.value = formattedMinutes;
     getSeconds.value = formattedSeconds;
-  }
+}
 
-function timer() {
-
-  function abc() {
+// Countdown function to update the timer
+function countdown() {
+    // Check if the timer has reached zero minutes and zero seconds
     if (minutes === 0 && seconds === 0) {
-      clearInterval(intervalId);
-      window.alert("done");
-    } else {
-      if (seconds === 0) {
-        if (minutes > 0) {
-          minutes--;
-          seconds = 59;
-        }
-      }
-      seconds--;
+        const mediaDiv = document.getElementById("media");
+        mediaDiv.innerHTML = '<img src="images/start.png" alt="Start">';
+        audio.pause();
+        return;
     }
 
+    // Decrement seconds and minutes as needed
+    if (seconds === 0) {
+        if (minutes > 0) {
+            minutes--;
+            seconds = 59;
+        }
+    } else {
+        seconds--;
+    }
+
+    // Update the timer display
     updateTimerDisplay();
-  }
-  intervalId = setInterval(abc, 1000);
+
+    // Call the countdown function again after 1 second
+    timeoutId = setTimeout(countdown, 1000);
 }
 
+// Function to start the timer
 function startTimer() {
-  minutes = parseInt(getMinutes.value)||0;
-  seconds = parseInt(getSeconds.value)||0;
-  timer();
+    minutes = parseInt(getMinutes.value) || 0;
+    seconds = parseInt(getSeconds.value) || 0;
+    if (minutes === 0 && seconds === 0) {
+        alert("Timer Not Set");
+        return;
+    }
+    const mediaDiv = document.getElementById("media");
+    mediaDiv.innerHTML = '<img src="images/running.gif" alt="Running">';
+    audio.play();
+    countdown(); // Start the countdown
 }
 
+// Function to stop the timer
 function stopTimer() {
-  clearInterval(intervalId);
-  intervalId = null;
+    const mediaDiv = document.getElementById("media");
+    mediaDiv.innerHTML = '<img src="images/reset.png" alt="Start">';
+    audio.pause();
+    clearTimeout(timeoutId); // Stop the timer
 }
 
+// Function to reset the timer
 function resetTimer() {
-  stopTimer();
-  minutes = 0;
-  seconds = 0;
-  getMinutes.value = "00";
-  getSeconds.value = "00";
+    const mediaDiv = document.getElementById("media");
+    mediaDiv.innerHTML = '<img src="images/reset.png" alt="Reset">';
+    stopTimer(); // Stop the timer
+    audio.pause();
+    minutes = 0;
+    seconds = 0;
+    getMinutes.value = "00";
+    getSeconds.value = "00";
 }
 
-
-
+// Event listeners for buttons
 startBtn.addEventListener("click", startTimer);
 stopBtn.addEventListener("click", stopTimer);
 resetBtn.addEventListener("click", resetTimer);
